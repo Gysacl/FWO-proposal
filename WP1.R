01615429eabd044dd1b8e3bb255063a9d6dcf580
Gysacl <gysa.langelet@hotmail.be>
2026-02-19 12:11:40 +0100

    first try maps

--- WP1.R
+++ WP1.R
@@ -1,5 +1,4 @@
 library(here)
 
-readLines("fc_VIS_link_obs.txt", n = 1)
-VMMlink = read.table(here("fc_VIS_link_obs.txt"), sep = " \", header = TRUE)
+VMMlink = read.table(here("fc_VIS_link_obs.txt"), sep = " \", header = TRUE )
 
