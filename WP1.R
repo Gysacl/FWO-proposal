b40a4b9f16dca6589486187a8af47ab8a02b773c
Gysacl <gysa.langelet@hotmail.be>
2026-02-21 21:30:22 +0100

    maps

--- WP1.R
+++ WP1.R
@@ -17,9 +17,15 @@ VMMlink <- read.table("fc_VIS_link_obs.csv", header = FALSE, sep = " ", stringsA
   VMMlink$V8 <- NULL
   VMMlink <- VMMlink[ , -1]
   colnames(VMMlink) <- c("VispuntWID", "WaarnemingWID", "Datum", "VispuntX", "VispuntY", "geometry")
+VMMlink <- VMMlink[ , -2]
+VMMlink <- VMMlink[ , -2]
+VMMlink <- unique( VMMlink )
+
   
 shape <- read_sf(here("world-administrative-boundaries.shp"))
 Belgium <- shape %>% filter(ISO_3_terri == "BEL")
+shape2 <- read_sf(here("georef-belgium-region-millesime.shp"))
+Vlaanderen <- shape2[ 1, ]
 
 ## datasets to shapefiles 
 VMMbiotasf <- st_as_sf(VMMbiota, coords = c("Lambert.X", "Lambert.Y"), crs = 3812)
@@ -30,26 +36,45 @@ VMMlinksf <- st_as_sf(VMMlink, coords = c("VispuntX","VispuntY"), crs = 31370)
 
 ##  maps tmap 
 # complete overview 
-tmap_mode("view")+
-  tm_shape(Belgium)+
+tmap_mode("plot")+
+  tm_shape(Vlaanderen)+
   tm_lines(lwd = 5)+
   tm_shape(VMMlinksf)+
-  tm_dots(fill_alpha = 0.5, fill = "red", size = 0.7)+
+  tm_dots(fill_alpha = 0.8, fill = "red", size = 1)+
   tm_shape(VMMbiotasf)+
   tm_dots(fill_alpha = 0.8, fill = "blue", size = 0.7)
 
 
 # biota 
 tmap_mode("plot")+
-  tm_shape(Belgium)+
+  tm_shape(Vlaanderen)+
   tm_lines(lwd = 5)+
   tm_shape(VMMbiotasf)+
-  tm_dots(fill_alpha = 0.5, fill = "blue", size = 0.7)
+  tm_dots(fill_alpha = 1, fill = "#009E73", size = 0.7)
 
 # VMM(fysico-chemie)  +  VIS --> al gekoppeld! 
 tmap_mode("plot")+
-  tm_shape(Belgium)+
+  tm_shape(Vlaanderen)+
   tm_lines(lwd = 5)+
   tm_shape(VMMlinksf)+
-  tm_dots(fill_alpha = 0.5, fill = "red", size = 0.7)
+  tm_dots(fill_alpha = 1, fill = "#CC79A7", size = 0.7)
 
+# VMM(fysico-chemie)  +  VIS --> al gekoppeld! 
+tmap_mode("view")+
+  tm_shape(Vlaanderen)+
+  tm_lines(lwd = 5)+
+  tm_shape(VMMlinksf)+
+  tm_dots(fill_alpha = 0.2, fill = "red", size = 1)+
+  tm_shape(VMMbiotasf)+
+  tm_dots(fill_alpha = 0.8, fill = "blue", size = 0.7)+
+  tm_shape(join)+
+  tm_dots(fill_alpha = 0.8, fill = "yellow", size = 0.7)
+
+# testje overlappende punten --> fysico-chemi + biota! 
+VMMbiotasf <- st_transform(VMMbiotasf, st_crs(VMMlinksf))
+join <- st_join(
+  VMMlinksf,
+  VMMbiotasf,
+  join = st_is_within_distance,
+  dist = 100)
+join <- na.omit(join)
